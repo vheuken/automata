8ca8cf0316645992b1c96ee8e6b923fcf1be834c
Vincent Heuken <me@vincentheuken.com>
2015-04-14 08:55:49 -0700

    added generated project stuff

---
+++ project.clj
@@ -0,0 +1,34 @@
+(defproject clojuremata "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2755"]]
+
+  :node-dependencies [[source-map-support "0.2.8"]]
+
+  :plugins [[lein-cljsbuild "1.0.4"]
+            [lein-npm "0.4.0"]]
+
+  :source-paths ["src" "target/classes"]
+
+  :clean-targets ["out" "out-adv"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :compiler {
+                :main clojuremata.core
+                :output-to "out/clojuremata.js"
+                :output-dir "out"
+                :optimizations :none
+                :cache-analysis true
+                :source-map true}}
+             {:id "release"
+              :source-paths ["src"]
+              :compiler {
+                :main clojuremata.core
+                :output-to "out-adv/clojuremata.min.js"
+                :output-dir "out-adv"
+                :optimizations :advanced
+                :pretty-print false}}]})
